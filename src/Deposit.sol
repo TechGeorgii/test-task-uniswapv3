f3bb934bf8ac1b066f11690e9de0f25305aff64d
TechGeorgii <master.wsd064@gmail.com>
2024-09-30 19:45:51 +0700

    Deposit contract. Pool state was retrieved successfully!

--- src/Deposit.sol
+++ src/Deposit.sol
@@ -2,22 +2,61 @@
 pragma solidity ^0.8.13;
 
 import "./lib/TickMath.sol";
-import "./lib/INonfungiblePositionManager.sol";
+import "./lib/interfaces/INonfungiblePositionManager.sol";
+import "./lib/interfaces/IUniswapV3Pool.sol";
 import {console} from "forge-std/console.sol";
 
 contract Deposit {
+    struct PoolSlot {
+        
+        uint160 sqrtPriceX96;
+        int24 tick;
+        uint16 observationIndex;
+        uint16 observationCardinality;
+        uint16 observationCardinalityNext;
+        uint8 feeProtocol;
+        bool unlocked;        
+    }
+
     int24 min_tick = -887272;
     int24 max_tick = 887272;
     address internal constant NON_FUNGIBLE_POS_MANAGER = 0xC36442b4a4522E871399CD717aBDD847Ab11FE88;
 
-    function createPosition(address pool, uint256 amount0Desired, uint256 amount1Desired, uint16 width) external returns (
+    function createPosition(address pool, uint256 amount0Desired, uint256 amount1Desired, uint16 width, uint256 deadline) external returns (
             uint256 tokenId,
             uint128 liquidity,
             uint256 amount0,
             uint256 amount1
-        )
-        {
-        
-        INonfungiblePositionManager _manager = INonfungiblePositionManager(NON_FUNGIBLE_POS_MANAGER);
+        ) {
+            require(width >= 0 && width <= 10000, "width");
+
+            IUniswapV3Pool _pool = IUniswapV3Pool(pool);
+            (  
+                uint160 sqrtPriceX96,  
+                int24 tick,  
+                ,  
+                ,  
+                ,  
+                ,  
+                  
+            )  = _pool.slot0();
+            console.log("tick:", tick);
+            console.log("sqrtPriceX96:", sqrtPriceX96);
+
+            //INonfungiblePositionManager _manager = INonfungiblePositionManager(NON_FUNGIBLE_POS_MANAGER);
+
+            // INonfungiblePositionManager.MintParams memory _params;
+            // _params.token0 = _pool.token0();
+            // _params.token1 = _pool.token1();
+            // _params.fee = _pool.fee();
+            // _params.recipient = 0xB0b12f40b18027f1a2074D2Ab11C6e0d6c6acbB5;
+            // _params.amount0Desired = amount0Desired;
+            // _params.amount1Desired = amount1Desired;
+            // _params.deadline = deadline;
+            
+            // return _manager.mint(_params);
+
+            return (0, 0, 0, 0);
     }
 }
+
---
+++ test/Deposit.t.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {Test, console} from "forge-std/Test.sol";
+import {Deposit} from "../src/Deposit.sol";
+import {console} from "forge-std/console.sol";
+
+contract DepositTest is Test {
+    address constant OPTIMISM_USDT_DAI_POOL = 0x8323D063b1D12ACce4742f1E3ed9BC46d71f4222;
+    Deposit public deposit;
+
+    function setUp() public {
+        deposit = new Deposit();
+    }
+
+    function test_Calc() public {
+        // from tx 0x3c6da60ab1f78c635a9a27a84163c0ec628111c370d2073246e44adf728a3322
+        deposit.createPosition(OPTIMISM_USDT_DAI_POOL, 1878690348, 1598506205772095349654, 1000, 999999999999);
+    }
+}
